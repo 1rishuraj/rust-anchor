911a46ead2b29d17b91c2fc3685cb70245f48a87
Rishu Raj <rishuraj1702@gmail.com>
2026-03-25 18:39:26 +0530

    rectified bug for "a b c d e " string

--- rust-lifetimes/src/lib.rs
+++ rust-lifetimes/src/lib.rs
@@ -7,43 +7,46 @@
 /// Custom string splitter iterator.
 #[derive(Debug)]
 pub struct StrSplit<'a> {
-    remainder: &'a str,
+    remainder: Option<&'a str>,
     delimiter: &'a str,
 }
 
 impl<'a> StrSplit<'a> {
     pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
         Self {
-            remainder: haystack,
+            remainder: Some(haystack),
             delimiter,
         }
     }
 }
 
 impl<'a> Iterator for StrSplit<'a> {
-    type Item = &'a str;
+    type Item =  &'a str;
 
     /// Advances the iterator and returns the next substring.
     fn next(&mut self) -> Option<Self::Item> {
-        if self.remainder.is_empty() {
-            return None;
+        let remaining = self.remainder.as_mut()?;
+        if let Some(delimeter_idx) = remaining.find(" "){
+            let until_delim:&'a str = &remaining[..delimeter_idx];
+            self.remainder = Some(&remaining[delimeter_idx+self.delimiter.len()..]);
+            Some(until_delim)
+        }else{
+            self.remainder.take()
         }
 
-        if let Some(next_delim) = self.remainder.find(self.delimiter) {
-            let until_delimiter = &self.remainder[..next_delim];
-            self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
-            Some(until_delimiter)
-        } else {
-            let rest = self.remainder;
-            self.remainder = "";
-            Some(rest)
-        }
     }
 }
 
 #[test]
 fn it_works() {
     let haystack = "a b c d e";
+    let letters = StrSplit::new(haystack, " ").collect::<Vec<_>>();
+    let letters2: Vec<&str> = haystack.split(" ").collect();
+    assert_eq!(letters, letters2);
+}
+#[test]
+fn it_works2() {
+    let haystack = "a b c d e ";
     let letters = StrSplit::new(haystack, " ").collect::<Vec<_>>();
     let letters2: Vec<&str> = haystack.split(" ").collect();
     assert_eq!(letters, letters2);
